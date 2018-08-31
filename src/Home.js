c98ae596010322b6274bb6fc15c0b36133ea64d9
yejun <yejun@kingsoft.com>
2018-08-31 14:24:52 +0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>http://dl.qin.ksyun.com/app/local-km-ksyun.tar.gz</p>
+        <p> <a href="http://dl.qin.ksyun.com/app/local-km-ksyun.tar.gz">点此下载</a> </p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
